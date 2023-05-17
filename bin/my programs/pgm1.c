2f27d91d1a6ad08ac69e820e1b4c324c5291f105
தமிழ்நேரம் <67970539+TamilNeram@users.noreply.github.com>
2023-05-17 14:45:01 +0530

    DM

---
+++ bin/my programs/pgm1.c
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main(void)
+{ 
+  int a;
+ printf("enter the value of a:");
+ scanf("%d",&a);
+ printf("\n hai - %d",a);
+ getch();
+return 0;
+   };
+  
